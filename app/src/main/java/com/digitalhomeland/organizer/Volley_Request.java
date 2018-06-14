ca45f08630531fb7938922e338a9def3054b020c
Shivam Pathak <pathak.shivambareilly@gmail.com>
2018-06-15 02:10:59 +0530

    code upload

---
+++ app/src/main/java/com/digitalhomeland/organizer/Volley_Request.java
@@ -0,0 +1,166 @@
+package com.digitalhomeland.organizer;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Asus on 10/25/2017.
+ */
+
+public class Volley_Request {
+
+    static String responseString = "";
+
+    public static void createRequest(Context mContext, String requestURI, String type, String returnPath, final String requestBody) {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+        if (type.equals("GET")) {
+            Log.d("myTag", "inside get req");
+            // Initialize a new JsonObjectRequest instance
+            final String returnPathCopy = returnPath;
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    requestURI,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // Do something with response
+
+                            for (int i = 0; i < 10; i++) {
+                                //Log.d("myTag", "onResponse len : " + response.length());
+                                if (response != null) {
+                                    responseString = response.toString();
+                                    Log.d("myTag", "responsestring != 1 : " + responseString);
+
+                                } else {
+                                    try {
+                                        Thread.sleep(2000);
+                                    } catch (Exception e) {
+                                        Log.d("myTag", "error in sleep", e);
+                                    }
+                                }
+                            }
+
+                            //Log.d("myTag", "response : " + response.toString());
+//responseString = response.toString();
+                            // Log.d("myTag", "response assigned : " + responseString.toString());
+                            //intent for Create Notification Page
+                            // Intent i = new Intent(LandingActivity.this, SignupActivity.class);
+                            // i.putExtra("input",response.toString());
+                            //  i.putExtra("schoolId", "pihu007");
+                            // startActivity(i);
+                            // citySpinnerLoader(response.toString());
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // Do something when error occurred
+                            Log.d("myTag", "error aaya : " + error.toString());
+                        }
+                    }
+            );
+            jsonObjectRequest.setTag("GET");
+            requestQueue.cancelAll(new RequestQueue.RequestFilter() {
+                @Override
+                public boolean apply(Request<?> request) {
+                    Log.d("DEBUG","request running: "+request.getTag().toString());
+                    return true;
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } else if (type.equals("POST")) {
+            Log.d("myTag", "requesturi : " + requestURI + " : " + requestBody);
+            //Log.d("myTag", "requesturi : " + requestURI);
+            final String returnPathCopy = returnPath;
+            JSONObject requestObject = null;
+            try {
+                requestObject = new JSONObject(requestBody);
+            } catch (Exception e) {
+            }
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.POST,
+                    requestURI,
+                    requestObject,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // Do something with response
+                                //Log.d("myTag", "onResponse len : " + response.length());
+                                if (response != null) {
+                                    responseString = response.toString();
+                                    Log.d("myTag", "responsestring != 1 : " + responseString);
+                                    if (returnPathCopy == "addStore")
+                                        LandingActivity.addStoreResponse(responseString);
+                                    else if (returnPathCopy == "loadStore")
+                                        LandingActivity.getStoresResponse(responseString);
+                                    else if (returnPathCopy == "SignActivityEmployee")
+                                        SignupActivity.getSignUpResponse(responseString);
+                                    else if (returnPathCopy == "getEmployees"){}
+                                        //DashboardActivity.getEmpResponse(responseString);
+                                    else if (returnPathCopy == "addTeam")
+                                        DashboardActivity.addTeamResponse(responseString);
+                                    else if (returnPathCopy == "getUnapprovedEmp")
+                                        DashboardActivity.getUnapprovedResponse(responseString);
+                                    else if (returnPathCopy == "ApproveEmployee")
+                                        EmployeeApproval.getApprovalResponse(responseString);
+                                    else if (returnPathCopy == "updateName")
+                                        EditTeam.updateNameResponse(responseString);
+                                    else if (returnPathCopy == "updateManager")
+                                        EditTeam.updateManagerResponse(responseString);
+                                } else {
+                                    try {
+                                        Thread.sleep(2000);
+                                    } catch (Exception e) {
+                                        Log.d("myTag", "error in sleep", e);
+                                    }
+                                }
+
+                            //Log.d("myTag", "response : " + response.toString());
+//responseString = response.toString();
+                            // Log.d("myTag", "response assigned : " + responseString.toString());
+
+                            //intent for Create Notification Page
+                            // Intent i = new Intent(LandingActivity.this, SignupActivity.class);
+
+                            // i.putExtra("input",response.toString());
+                            //  i.putExtra("schoolId", "pihu007");
+                            // startActivity(i);
+                            // citySpinnerLoader(response.toString());
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // Do something when error occurred
+                            error.printStackTrace();
+                            //String errorString = VolleyErrorHelper.getMessage(error, context);
+                            Log.d("myTag", "error aaya : " + error.toString());
+                        }
+                    }
+
+            );
+            requestQueue.cancelAll(new RequestQueue.RequestFilter() {
+                @Override
+                public boolean apply(Request<?> request) {
+                    Log.d("DEBUG","request running: "+request.getTag().toString());
+                    return true;
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/digitalhomeland/organizer/models/Employee.java
@@ -0,0 +1,93 @@
+package com.digitalhomeland.organizer.models;
+
+/**
+ * Created by Asus on 2/3/2018.
+ */
+
+public class Employee {
+    String _id;
+    String firstName;
+    String middleName;
+    String lastName;
+    String phone;
+    String email;
+    String aadharId;
+    String employeeId;
+    String isManager;
+    String teamName;
+    String designation;
+    String managerId;
+
+
+    public Employee(String _id, String firstName, String middleName, String lastName, String phone, String email,String aadharId, String employeeId, String isManager)
+    {
+        this._id = _id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.aadharId = aadharId;
+        this.employeeId = employeeId;
+        this.isManager = isManager;
+        this.teamName = "";
+        this.designation = "";
+        this.managerId = "";
+    }
+
+    public Employee( String firstName, String middleName, String lastName, String phone, String email,String aadharId, String employeeId, String isManager)
+    {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.aadharId = aadharId;
+        this.employeeId = employeeId;
+        this.isManager = isManager;
+        this.teamName = "";
+        this.designation = "";
+        this.managerId = "";
+    }
+
+    public Employee(String _id, String firstName, String middleName, String lastName, String phone, String email,String aadharId, String employeeId, String isManger, String teamName, String designation, String managerId)
+    {
+        this._id = _id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.aadharId = aadharId;
+        this.employeeId = employeeId;
+        this.isManager = isManger;
+        this.teamName = teamName;
+        this.designation = designation;
+        this.managerId = managerId;
+    }
+
+    public String get_id(){return _id;}
+    public String getFirstName(){return  firstName;}
+    public String getMiddleName() {return middleName;}
+    public String getLastName() {return lastName;}
+    public String getPhone() {return phone;}
+    public String getEmail() {return email;}
+    public String getAadharId() {return aadharId;}
+    public String getEmployeeId() { return employeeId;}
+
+    public String getIsManager() {
+        return isManager;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public String getManagerId() {
+        return managerId;
+    }
+}
